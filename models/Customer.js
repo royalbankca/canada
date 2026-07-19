0e31443e793968a690ade46481d4d87688929bf5
Royal Bank <brucea.heyman@gmail.com>
2026-07-19 13:04:16 +0000

    Update Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -1,50 +1,70 @@
 const mongoose = require("mongoose");
 
-const customerSchema = new mongoose.Schema(
-{
+const customerSchema = new mongoose.Schema({
+
     firstName: {
         type: String,
         required: true
     },
+
     lastName: {
         type: String,
         required: true
     },
+
     email: {
         type: String,
         required: true,
         unique: true
     },
+
     phone: {
         type: String,
         required: true
     },
-    country: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    accountNumber: {
-        type: String,
-        unique: true
-    },
-    balance: {
-        type: Number,
-        default: 0
-    },
+
+    birthDate: String,
+
+    gender: String,
+
+    nationality: String,
+
+    profession: String,
+
+    country: String,
+
+    city: String,
+
+    address: String,
+
+    accountType: String,
+
     currency: {
         type: String,
         default: "CAD"
     },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    accountNumber: {
+        type: String,
+        unique: true
+    },
+
+    balance: {
+        type: Number,
+        default: 0
+    },
+
     status: {
         type: String,
         default: "Active"
     }
-},
-{
+
+}, {
     timestamps: true
 });
 
